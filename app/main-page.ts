831473fc7ee74c79f876470d788ce0d4513c0612
manish.k <manish.k@v2solutions.com>
2018-06-26 17:08:43 +0530

    added image digit feature

--- app/main-page.ts
+++ app/main-page.ts
@@ -2,9 +2,26 @@ import { EventData } from 'data/observable';
 import { Page } from 'ui/page';
 import { HelloWorldModel } from './main-view-model';
 
+let page;
+var hwm;
+
 // Event handler for Page "navigatingTo" event attached in main-page.xml
 export function navigatingTo(args: EventData) {
 
-    let page = <Page>args.object;
-    page.bindingContext = new HelloWorldModel();
+    page = <Page>args.object;
+    hwm = new HelloWorldModel();
+
+
+    console.log("hwm.minutes " + hwm.minutes);
+
+    hwm.set("myH0Src", "~/images/0.png");
+    hwm.set("myH1Src", "~/images/0.png");
+
+    hwm.set("myM0Src", "~/images/0.png");
+    hwm.set("myM1Src", "~/images/0.png");
+
+    hwm.set("myS0Src", "~/images/0.png");
+    hwm.set("myS1Src", "~/images/0.png");
+
+    page.bindingContext = hwm;
 }
--- app/main-view-model.ts
+++ app/main-view-model.ts
@@ -1,4 +1,5 @@
 import { Observable } from 'data/observable';
+const ObservableArray = require("data/observable-array").ObservableArray;
 
 export class HelloWorldModel extends Observable {
 
@@ -7,16 +8,27 @@ export class HelloWorldModel extends Observable {
     private _message: string;
     private timer;
 
+    public hours = "0";
+    public minutes = "0";
+    public seconds = "0";
+
+    public data = new ObservableArray([
+        { count: "3250", date: "25/06/2018" },
+    ]);
+
 
     constructor() {
         super();
         this.initialize();
+
     }
 
     initialize() {
         // Initialize default values.
-        this._counter = 0;
-
+        this._counter = 0; // 82790;
+        this.hours = "0";
+        this.minutes = "0";
+        this.seconds = "0";
         this.updateMessage();
         this._isStarted = false;
     }
@@ -63,22 +75,58 @@ export class HelloWorldModel extends Observable {
 
     private updateMessage() {
 
-        var hours = Math.floor(this._counter / 3600).toString();
-        var minutes = Math.floor((this._counter / 60) % 60).toString();
-        var seconds = Math.floor(this._counter % 60).toString();
+        this.hours = Math.floor(this._counter / 3600).toString();
+        this.minutes = Math.floor((this._counter / 60) % 60).toString();
+        this.seconds = Math.floor(this._counter % 60).toString();
 
-        if (hours.length < 2) {
-            hours = '0' + hours;
+        this.updateClockDisplay();
+
+        this.message = `${this.hours}:${this.minutes}:${this.seconds}`;
+    }
+    public convertValueToTime(value: number): string {
+        var hour = Math.floor(value / 3600).toString();
+        var minute = Math.floor((value / 60) % 60).toString();
+        var second = Math.floor(value % 60).toString();
+
+        return hour + ":" + minute + ":" + second;
+
+    }
+    private updateClockDisplay() {
+        if (this.hours.length < 2) {
+            this.set("myH1Src", "~/images/" + this.hours + ".png");
+            this.hours = '0' + this.hours;
+            this.set("myH0Src", "~/images/0.png");
+        } else {
+            let h0 = this.hours.substr(0, 1);
+            let h1 = this.hours.substr(1, 1);
+
+            this.set("myH1Src", "~/images/" + h1 + ".png");
+            this.set("myH0Src", "~/images/" + h0 + ".png");
         }
 
-        if (minutes.length < 2) {
-            minutes = '0' + minutes;
+        if (this.minutes.length < 2) {
+            this.set("myM1Src", "~/images/" + this.minutes + ".png");
+            this.minutes = '0' + this.minutes;
+            this.set("myM0Src", "~/images/0.png");
+        } else {
+            let m0 = this.minutes.substr(0, 1);
+            let m1 = this.minutes.substr(1, 1);
+
+            this.set("myM1Src", "~/images/" + m1 + ".png");
+            this.set("myM0Src", "~/images/" + m0 + ".png");
         }
 
-        if (seconds.length < 2) {
-            seconds = '0' + seconds;
-        }
 
-        this.message = `${hours}:${minutes}:${seconds}`;
+        if (this.seconds.length < 2) {
+            this.set("myS1Src", "~/images/" + this.seconds + ".png");
+            this.seconds = '0' + this.seconds;
+            this.set("myS0Src", "~/images/0.png");
+        } else {
+            let s0 = this.seconds.substr(0, 1);
+            let s1 = this.seconds.substr(1, 1);
+
+            this.set("myS1Src", "~/images/" + s1 + ".png");
+            this.set("myS0Src", "~/images/" + s0 + ".png");
+        }
     }
 }
